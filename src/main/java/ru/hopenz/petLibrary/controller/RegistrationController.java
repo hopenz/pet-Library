7feae11777cbb9f0d50ee69db3751ccd6bf8780f
hopenz <petrushina_03@inbox.ru>
2025-03-07 13:29:39 +0500

    Вернула регистрацию/авторизацию

--- src/main/java/ru/hopenz/petLibrary/controller/RegistrationController.java
+++ src/main/java/ru/hopenz/petLibrary/controller/RegistrationController.java
@@ -1,31 +1,36 @@
 package ru.hopenz.petLibrary.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.hopenz.petLibrary.data.entity.User;
+import ru.hopenz.petLibrary.service.UserService;
 
 @Controller
 public class RegistrationController {
 
-    //private final UserService userService;
+    private final UserService userService;
 
-//    public RegistrationController(UserService userService) {
-//        this.userService = userService;
-//    }
-//
-//    @GetMapping("/registration")
-//    public String registration(Model model) {
-//        model.addAttribute("user", new User());
-//        return "registration";
-//    }
-//
-//    @PostMapping("/registration")
-//    public String adduser(Model model, @ModelAttribute User user) {
-//        if (!userService.addUser(user)) {
-//            model.addAttribute("message", "User exists");
-//            return "registration";
-//        }
-//        return "redirect:/login";
-//    }
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String adduser(Model model, @ModelAttribute User user) {
+        if (!userService.addUser(user)) {
+            model.addAttribute("message", "User exists");
+            return "registration";
+        }
+        return "redirect:/login";
+    }
 
     @GetMapping("/")
     public String menu() {
--- src/main/java/ru/hopenz/petLibrary/repository/UserRepository.java
+++ src/main/java/ru/hopenz/petLibrary/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package ru.hopenz.petLibrary.repository;
 
-//@Repository
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.hopenz.petLibrary.data.entity.Book;
+import ru.hopenz.petLibrary.data.entity.User;
 
-    //User findByUsername(String username);
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
 }
